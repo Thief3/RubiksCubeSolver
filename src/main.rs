fd55957290bf1a665ad1ef18bfea0fc94a526e5c
Malik Kissarli <kissarlim@gmail.com>
2018-08-30 23:58:40 +0100

    Basic tests

--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,6 @@ fn main() {
     //let mut vertical_faces = [rubiks.front, rubiks.bottom, rubiks.back, rubiks.top];
 
     rubiks.top = rotate_face_clockwise([0,1,2,3,4,5,6,7,8]);
-
-    println!("{:?}", rubiks.top);
-
 }
 
 /// Roates a rubiks cube face clockwise.
@@ -37,4 +34,23 @@ fn rotate_face_anticlockwise (face: [i32; 9]) -> [i32; 9]{
 	[face[8], face[5], face[2],
 	face[7], face[4], face[1],
 	face[6], face[3], face[0]]
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_rotate_face_clockwise() {
+		assert_eq!(
+			[0, 3, 6, 1, 4, 7, 2, 5, 8],
+			rotate_face_clockwise([0, 1, 2, 3, 4, 5, 6, 7, 8]));
+	}
+
+	#[test]
+	fn test_rotate_face_anticlockwise() {
+		assert_eq!(
+			[8, 5, 2, 7, 4, 1, 6, 3, 0],
+			rotate_face_anticlockwise([0, 1, 2, 3, 4, 5, 6, 7, 8]));
+	}
 }
