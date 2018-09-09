0c6504309a2ddac608a4e313caa96d3df20f66e4
Malik Kissarli <kissarlim@gmail.com>
2018-09-09 13:02:26 +0100

    Added corner moves. They are statics as they are likely to be used a lot and memory<speed.

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,31 @@ impl Cube {
             self.corners[i].f();
         }
     }
+    fn b(&mut self) {
+        for i in 0..7 {
+            self.corners[i].b();
+        }
+    }
+    fn l(&mut self) {
+        for i in 0..7 {
+            self.corners[i].l();
+        }
+    }
+    fn r(&mut self) {
+        for i in 0..7 {
+            self.corners[i].r();
+        }
+    }
+    fn u(&mut self) {
+        for i in 0..7 {
+            self.corners[i].u();
+        }
+    }
+    fn d(&mut self) {
+        for i in 0..7 {
+            self.corners[i].d();
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -54,45 +79,136 @@ impl CornerCubie {
         a
     }
 
-    fn f(&mut self) {
+    fn movement(&mut self, corners: &[Corner; 8], orientation_change: [i32; 8]) {
         match self.coordinate {
             Corner::URF => {
-                self.coordinate = Corner::UFL;
-                self.orientation = (self.orientation + 1) % 3;
+                self.coordinate = corners[0];
+                self.orientation = (self.orientation + orientation_change[0]) % 3;
             }
             Corner::UFL => {
-                self.coordinate = Corner::DLF;
-                self.orientation = (self.orientation + 2) % 3;
+                self.coordinate = corners[1];
+                self.orientation = (self.orientation + orientation_change[1]) % 3;
             }
             Corner::ULB => {
-                self.coordinate = Corner::ULB;
-                self.orientation = (self.orientation + 0) % 3;
+                self.coordinate = corners[2];
+                self.orientation = (self.orientation + orientation_change[2]) % 3;
             }
             Corner::UBR => {
-                self.coordinate = Corner::UBR;
-                self.orientation = (self.orientation + 0) % 3;
+                self.coordinate = corners[3];
+                self.orientation = (self.orientation + orientation_change[3]) % 3;
             }
             Corner::DFR => {
-                self.coordinate = Corner::URF;
-                self.orientation = (self.orientation + 2) % 3;
+                self.coordinate = corners[4];
+                self.orientation = (self.orientation + orientation_change[4]) % 3;
             }
             Corner::DLF => {
-                self.coordinate = Corner::DFR;
-                self.orientation = (self.orientation + 1) % 3;
+                self.coordinate = corners[5];
+                self.orientation = (self.orientation + orientation_change[5]) % 3;
             }
             Corner::DBL => {
-                self.coordinate = Corner::DBL;
-                self.orientation = (self.orientation + 0) % 3;
+                self.coordinate = corners[6];
+                self.orientation = (self.orientation + orientation_change[6]) % 3;
             }
             Corner::DRB => {
-                self.coordinate = Corner::DRB;
-                self.orientation = (self.orientation + 0) % 3;
+                self.coordinate = corners[7];
+                self.orientation = (self.orientation + orientation_change[7]) % 3;
             }
         };
     }
+
+    fn f(&mut self) {
+        self.movement(&F_CORNER_TRANSFORM, F_CORNER_ORIENTATION_TRANSFORM)
+    }
+    fn b(&mut self) {
+        self.movement(&B_CORNER_TRANSFORM, B_CORNER_ORIENTATION_TRANSFORM)
+    }
+    fn r(&mut self) {
+        self.movement(&R_CORNER_TRANSFORM, R_CORNER_ORIENTATION_TRANSFORM)
+    }
+    fn l(&mut self) {
+        self.movement(&L_CORNER_TRANSFORM, L_CORNER_ORIENTATION_TRANSFORM)
+    }
+    fn u(&mut self) {
+        self.movement(&U_CORNER_TRANSFORM, U_CORNER_ORIENTATION_TRANSFORM)
+    }
+    fn d(&mut self) {
+        self.movement(&D_CORNER_TRANSFORM, D_CORNER_ORIENTATION_TRANSFORM)
+    }
 }
 
-#[derive(Debug, PartialEq, PartialOrd)]
+static F_CORNER_TRANSFORM: [Corner; 8] = [
+    Corner::UFL,
+    Corner::DLF,
+    Corner::ULB,
+    Corner::UBR,
+    Corner::URF,
+    Corner::DFR,
+    Corner::DBL,
+    Corner::DRB,
+];
+static F_CORNER_ORIENTATION_TRANSFORM: [i32; 8] = [1, 2, 0, 0, 2, 1, 0, 0];
+
+static B_CORNER_TRANSFORM: [Corner; 8] = [
+    Corner::URF,
+    Corner::UFL,
+    Corner::UBR,
+    Corner::DRB,
+    Corner::DFR,
+    Corner::DLF,
+    Corner::ULB,
+    Corner::DBL,
+];
+static B_CORNER_ORIENTATION_TRANSFORM: [i32; 8] = [0, 0, 1, 2, 0, 0, 2, 1];
+
+static R_CORNER_TRANSFORM: [Corner; 8] = [
+    Corner::DFR,
+    Corner::UFL,
+    Corner::ULB,
+    Corner::URF,
+    Corner::DRB,
+    Corner::DLF,
+    Corner::DBL,
+    Corner::DRB,
+];
+static R_CORNER_ORIENTATION_TRANSFORM: [i32; 8] = [2, 0, 0, 1, 1, 0, 0, 2];
+
+static L_CORNER_TRANSFORM: [Corner; 8] = [
+    Corner::URF,
+    Corner::ULB,
+    Corner::DBL,
+    Corner::UBR,
+    Corner::DFR,
+    Corner::UFL,
+    Corner::DLF,
+    Corner::DRB,
+];
+static L_CORNER_ORIENTATION_TRANSFORM: [i32; 8] = [0, 1, 2, 0, 0, 2, 1, 0];
+
+static U_CORNER_TRANSFORM: [Corner; 8] = [
+    Corner::UBR,
+    Corner::URF,
+    Corner::UFL,
+    Corner::ULB,
+    Corner::DFR,
+    Corner::DLF,
+    Corner::DBL,
+    Corner::DRB,
+];
+static U_CORNER_ORIENTATION_TRANSFORM: [i32; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
+
+static D_CORNER_TRANSFORM: [Corner; 8] = [
+    Corner::URF,
+    Corner::UFL,
+    Corner::ULB,
+    Corner::UBR,
+    Corner::DLF,
+    Corner::DBL,
+    Corner::DRB,
+    Corner::DFR,
+];
+static D_CORNER_ORIENTATION_TRANSFORM: [i32; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
+
+#[derive(Debug, PartialEq, PartialOrd, Copy, Clone)]
 enum Corner {
     URF = 1,
     UFL,
@@ -122,9 +238,9 @@ enum Edge {
 
 fn main() {
     let mut c = Cube::new();
-    c.f();
-    c.f();
-    c.f();
-    c.f();
+    c.d();
+    c.d();
+    c.d();
+    c.d();
     println!("{:?}", c);
 }
